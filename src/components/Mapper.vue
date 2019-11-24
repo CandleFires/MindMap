28f951a8860eb2f2d1b325f3f60e969dbd3f6ea3
Radovan Balog <Radovan.Balog@2ring.com>
2019-11-24 12:57:47 +0100

    Init and auto reesize paper canvas

--- src/components/Mapper.vue
+++ src/components/Mapper.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
+    <div class="mapper-wrapper">
         <SubNav></SubNav>
-        <section class="mapper">
-            <canvas></canvas>
+        <section ref="mapper" class="mapper">
+            <canvas ref="canvas" width=800 height=600 resize />
         </section>
     </div>
 </template>
@@ -12,6 +12,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import { State } from 'vuex-class';
 import SubNav from './SubNav.vue';
 import IState from '../interfaces/IState';
+import paper, { Size, view } from 'paper';
 
 @Component({
     components: {
@@ -19,12 +20,40 @@ import IState from '../interfaces/IState';
     }
 })
 export default class Application extends Vue {
+    private canvas!: HTMLCanvasElement;
 
+    private mounted() {
+        this.canvas = this.$refs.canvas as HTMLCanvasElement;
+        paper.setup(this.canvas);
+        window.addEventListener('resize', this.resizeCavnas);
+        this.resizeCavnas();
+    }
+
+    private beforeDestroy() {
+        window.removeEventListener('resize', this.resizeCavnas);
+    }
+
+    private resizeCavnas() {
+        let navbarsSize = 0;
+        document.querySelectorAll('.navbar').forEach((navbar) => navbarsSize += navbar.clientHeight);
+        const canvasHeight = window.innerHeight - navbarsSize;
+        view.viewSize!.width = window.outerWidth;
+        view.viewSize!.height = canvasHeight;
+    }
 }
 </script>
 
 <style scoped lang="scss">
-.mapper {
+.mapper-wrapper {
     flex: 1 1 auto;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    .mapper {
+        flex: 1 1 auto;
+        canvas {
+            width: 100%;
+        }
+    }
 }
 </style>

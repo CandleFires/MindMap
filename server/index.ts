0e5dce6930cec03cb292a21fba4d9923b28b48f0
Radovan Balog <Radovan.Balog@2ring.com>
2019-12-01 17:42:38 +0100

    Static for built project

--- server/index.ts
+++ server/index.ts
@@ -24,7 +24,7 @@ async function appSetup(app: express.Application) {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
-    // app.use(express.static(path.resolve(__dirname, '../public')));
+    app.use(express.static(path.resolve(__dirname, '../dist')));
     app.set('port', port);
 }
 

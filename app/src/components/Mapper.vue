b6bb897c4c2a005a0c6feac2a2dc86b5f7564433
Radovan Balog <Radovan.Balog@2ring.com>
2019-12-01 18:57:39 +0100

    Change canvas to fabric

--- app/src/components/Mapper.vue
+++ app/src/components/Mapper.vue
@@ -2,7 +2,7 @@
     <div class="mapper-wrapper">
         <SubNav></SubNav>
         <section ref="mapper" class="mapper">
-            <canvas ref="canvas" width=800 height=600 resize />
+            <canvas ref="canvas" />
         </section>
     </div>
 </template>
@@ -12,7 +12,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import { State } from 'vuex-class';
 import SubNav from './SubNav.vue';
 import IState from '../interfaces/IState';
-import paper, { Size } from 'paper';
+import { fabric } from 'fabric';
 
 @Component({
     components: {
@@ -20,13 +20,19 @@ import paper, { Size } from 'paper';
     }
 })
 export default class Application extends Vue {
-    private canvas!: HTMLCanvasElement;
+    private canvas!: fabric.Canvas;
 
     private mounted() {
-        this.canvas = this.$refs.canvas as HTMLCanvasElement;
-        paper.setup(this.canvas);
+        this.canvas = new fabric.Canvas(this.$refs.canvas as HTMLCanvasElement);
         window.addEventListener('resize', this.resizeCavnas);
         this.resizeCavnas();
+        this.loadMap();
+    }
+
+    private loadMap() {
+        const center = this.canvas.getCenter();
+        this.canvas.add(this.ellipseAt(center.left, center.top, 200, 140));
+        this.canvas.renderAll();
     }
 
     private beforeDestroy() {
@@ -34,11 +40,26 @@ export default class Application extends Vue {
     }
 
     private resizeCavnas() {
-        let navbarsSize = 0;
-        document.querySelectorAll('.navbar').forEach((navbar) => navbarsSize += navbar.clientHeight);
-        const canvasHeight = window.innerHeight - navbarsSize;
-        paper.view.viewSize!.width = window.outerWidth;
-        paper.view.viewSize!.height = canvasHeight;
+        const container = this.$refs.mapper as HTMLElement;
+        this.canvas.setWidth(window.innerWidth);
+        this.canvas.setHeight(container.clientHeight);
+        this.canvas.calcOffset();
+    }
+
+    private ellipseAt(x: number, y: number, width: number, height: number): fabric.Ellipse {
+        console.warn(`creating ellipse at ${x} ${y} - ${width} ${height}`);
+        return new fabric.Ellipse({
+            originX: 'center',
+            originY: 'center',
+            left: x,
+            top: y,
+            rx: width / 2,
+            ry: height / 2,
+            strokeWidth: 1,
+            stroke: 'black',
+            fill: 'red',
+            hasControls: false
+        });
     }
 }
 </script>
@@ -51,6 +72,7 @@ export default class Application extends Vue {
     overflow: hidden;
     .mapper {
         flex: 1 1 auto;
+        width: 100%;
         canvas {
             width: 100%;
         }
